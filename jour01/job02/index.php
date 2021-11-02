a1fd8604c9994b6fc5371687e4b6d0fbc2fbf9fb
Meline Darcy <meline.darcy@laplateforme.com>
2021-11-02 10:39:37 +0100

    job 2 commit

---
+++ jour01/job02/index.php
@@ -0,0 +1,13 @@
+<?php
+    $str = 'Hello LaPlateforme!';
+
+    echo $str;
+    echo '<br>'; /* j'ai utilisé une variable 
+    pour afficher Hello LaPlateforme ( ce commentaire peut être
+    écrit sur plusieurs lignes*/
+
+
+
+
+    echo 'Hello LaPlateforme!'; // Aucune variable ( ce commentaire ne se ferme pas)
+?>
