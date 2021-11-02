9e6ab6d2dc727f9cb0dace8ae877bd55ff8757c4
Meline Darcy <meline.darcy@laplateforme.com>
2021-11-02 10:22:44 +0100

    1er commit

---
+++ jour01/job01/index.php
@@ -0,0 +1,29 @@
+<?php
+    $str = 'LaPlateforme ';
+    $str2 = 'Vive ';
+    $str3 = '!';
+    
+
+    echo $str2;
+    echo $str;
+    echo $str3;
+    echo "<br>";
+
+    $int = '6';
+
+    echo $int;
+    echo "<br>";
+
+    $resul = ($int + 4);
+
+    echo $resul;
+    echo "<br>";
+    $myBool = true;
+    
+    echo $myBool;
+
+    echo "<br>";
+    $myBool = false;
+    echo $myBool;
+
+?>
